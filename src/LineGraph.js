473e6f1be90b1f3b09c5d4073ade33fe25f9a0ea
JC <jctrilamanila@gmail.com>
2021-01-21 13:24:29 -0500

    Added timeline on graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -2,7 +2,55 @@ import React, { useEffect, useState } from 'react'
 import { Line } from "react-chartjs-2";
 import './LineGraph.css'
 
-const options={
+function LineGraph() {
+    const [ graphData, setGraphData ] = useState([]);
+    const data = 
+        [{
+            x: 14,
+            y: 15
+        }, {
+            x: 15,
+            y: 20
+        }
+        ]
+
+        const createMockData = () => {
+            let data = [];
+            let value = 50;
+            for(var i=0; i < 366; i++){
+                let date = new Date();
+                date.setHours(0,0,0,0);
+                date.setDate(i);
+                value += Math.round((Math.random() < 0.5 ? 1 : 0))
+                data.push({x: date, y: value});
+            }
+            setGraphData(data);
+        }
+
+        useEffect(()=>{
+            createMockData();
+        }, [])
+    return (
+        <div className="linegraph">
+            <Line 
+                data={{
+                    datasets: [
+                        {
+                            type: "line",
+                            data: graphData,
+                            backgroundColor: "black",
+                            borderColor: "#5AC53B",
+                            borderWidth: 2,
+                            pointBorderColor: 'rgba(0, 0, 0, 0)',
+                            pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+                            pointHoverBackgroundColor: '#5AC53B',
+                            pointHoverBorderColor: '#000000',
+                            pointHoverBorderWidth: 4,
+                            pointHoverRadius: 6,
+                        },
+                    ],
+                }}
+                options={{
                     maintainAspectRatio: false,
                     legend: {
                         display: false
@@ -30,61 +78,7 @@ const options={
                             }
                         }]
                     }
-                };
-
-function LineGraph() {
-    const [ graphData, setGraphData ] = useState([]);
-    const data = 
-        [{
-            x: 14,
-            y: 15
-        }, {
-            x: 15,
-            y: 20
-        },
-        {
-            x: 17,
-            y: 22
-        }
-        ]
-
-        const createMockData = () => {
-            let data = [];
-            let value = 50;
-            for(var i=0; i < 366; i++){
-                let date = new Date();
-                date.setHours(0,0,0,0);
-                date.setDate(i);
-                value += Math.round((Math.random() < 0.5 ? 1 : 0))
-                data.push({x: date, y: value});
-            }
-            setGraphData(data);
-        }
-
-        useEffect(()=>{
-            createMockData();
-        }, [])
-    return (
-        <div className="linegraph">
-            <Line 
-                data={{
-                    datasets: [
-                        {
-                            type: "line",
-                            backgroundColor: "black",
-                            borderColor: "#5AC53B",
-                            borderWidth: 2,
-                            pointBorderColor: 'rgba(0, 0, 0, 0)',
-                            pointBackgroundColor: 'rgba(0, 0, 0, 0)',
-                            pointHoverBackgroundColor: '#5AC53B',
-                            pointHoverBorderColor: '#000000',
-                            pointHoverBorderWidth: 4,
-                            pointHoverRadius: 6,
-                            data: data,
-                        },
-                    ],
                 }}
-                
             />
             
         </div>
--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Newsfeed.css'
 import LineGraph from './LineGraph'
+import TimeLine from './TimeLine'
+
+
 
 function Newsfeed() {
     return (
@@ -13,6 +16,7 @@ function Newsfeed() {
                     </div>
                     <div className="newsfeed_chart">
                        <LineGraph />
+                       <TimeLine />
                     </div>
                 </div>
             </div>
